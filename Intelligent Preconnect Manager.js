4ef4101069c452803a14a8d678b5bff060274291
waltornjenga <waiyakiwaltor@gmail.com>
2025-11-13 17:49:00 +0300

    add network connection detection and event listener

--- Intelligent Preconnect Manager.js
+++ Intelligent Preconnect Manager.js
@@ -3,5 +3,14 @@ class PreconnectManager {
         this.connections = new Map();
         this.maxConnections = 6;
         this.retryDelay = 2000;
+        
+        this.connection = navigator.connection;
+        this.setupNetworkAwareness();
+    }
+
+    setupNetworkAwareness() {
+        if (this.connection) {
+            this.connection.addEventListener('change', this.handleNetworkChange.bind(this));
+        }
     }
 }
